5a92aaf15b4d5239599d41b87dc797bfdaa0a49e
Anushka A. Bhasme <78693700+https-ab@users.noreply.github.com>
2025-09-24 10:41:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+let changeBtn = document.getElementById("change_button");
+let resetBtn = document.getElementById("reset");
+
+ function resetColors() {
+      let gridItems = document.querySelectorAll('.grid-item');
+      gridItems.forEach(item => {
+        item.style.backgroundColor = 'transparent';
+      });
+    }
+
+    changeBtn.addEventListener('click', () => {
+      let blockId = document.getElementById('block_id').value;
+      let color = document.getElementById('colour_id').value;
+
+      if (blockId < 1 || blockId > 9 || !document.getElementById(blockId)) {
+        alert('Please enter a valid block ID between 1 and 9.');
+        return;
+      }
+    
+      resetColors();
+      
+      document.getElementById(blockId).style.backgroundColor = color;
+    });
+
+    resetBtn.addEventListener('click', resetColors);
